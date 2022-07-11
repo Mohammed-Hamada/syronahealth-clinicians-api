8ae57547ad7b627c21e58908a94bf9d03ded8bac
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-07-11 18:35:37 +0300

    Fix eslint warnings

--- bin/run.ts
+++ bin/run.ts
@@ -1,30 +1,18 @@
 #!/usr/bin/env node
-import app from '../src/app';
 import debugFactory from 'debug';
-const debug = debugFactory('express-generator:server');
 import http from 'http';
+import app from '../src/app';
 import { PORT } from '../src/config';
 
-const port = normalizePort(PORT);
-app.set('port', port);
+const debug = debugFactory('express-generator:server');
+
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(
-    'App is running at http://localhost:%d in %s mode',
-    PORT,
-    app.get('env'),
-    '\nPress CTRL-C to stop\n'
-  );
-});
-server.on('error', onError);
-server.on('listening', onListening);
-
-function normalizePort(val: string | undefined) {
+function normalizePort(val: string | undefined): number | string | boolean {
   let port: number;
   if (typeof val === 'string') {
     port = parseInt(val, 10);
-    if (isNaN(port)) {
+    if (Number.isNaN(port)) {
       return val;
     }
     if (port >= 0) {
@@ -34,30 +22,43 @@ function normalizePort(val: string | undefined) {
   return false;
 }
 
-function onError(error: NodeJS.ErrnoException) {
+const port = normalizePort(PORT);
+app.set('port', port);
+
+function onError(error: NodeJS.ErrnoException): void {
   if (error?.syscall !== 'listen') {
     throw error;
   }
 
-  const bind: string =
-    typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  const bind: string = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+      console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+      console.error(`${bind} is already in use`);
       process.exit(1);
       break;
     default:
       throw error;
   }
 }
-function onListening() {
+
+function onListening(): void {
   const addr = server.address();
-  const bind: string =
-    typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
-  debug('Listening on ' + bind);
+  const bind: string = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr?.port}`;
+  debug(`Listening on ${bind}`);
 }
+
+server.listen(port, () => {
+  console.log(
+    'App is running at http://localhost:%d in %s mode',
+    PORT,
+    app.get('env'),
+    '\nPress CTRL-C to stop\n',
+  );
+});
+server.on('error', onError);
+server.on('listening', onListening);
