ae4f2af182b8ea17f3def4206b6d2b9440712720
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-08-01 10:29:53 +0300

    Create associations between the models

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,3 +1,27 @@
+import sequelize from '../connection';
+import Company from './Company';
+import Employee from './Employee';
 import User from './User';
+import UserState from './UserState';
 
-export default User;
+(async (): Promise<void> => {
+  try {
+    User.hasMany(UserState, { foreignKey: 'user' });
+
+    User.belongsToMany(Company, { through: Employee, foreignKey: 'user' });
+    Company.belongsToMany(User, { through: Employee, foreignKey: 'corporate' });
+
+    User.hasMany(Employee);
+    Company.hasMany(Employee);
+
+    Employee.belongsTo(User);
+    Employee.belongsTo(Company);
+
+    console.log('Database built successfully!');
+  } catch (error) {
+    console.log(error);
+  }
+})();
+export {
+  Company, Employee, User, UserState, sequelize,
+};
