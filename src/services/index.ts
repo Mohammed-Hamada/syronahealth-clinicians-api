8ecde60e6b239352b6fe2dc878ab0a2f43629617
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-08-11 14:19:03 +0300

    create function and refactor Model Query | handle when user have no engagements

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,13 @@
 import getAllCompanies from './getAllCompanies';
 import getCompanyById from './getCompanyById';
 import getUsersEngagementsForCompany from './getUsersEngagementsForCompany';
+import getUsersInterestsForCompany from './getUsersInterestsForCompany';
+import getUsersStatesFromModel from './getUsersStatesFromModel';
 
-export { getAllCompanies, getCompanyById, getUsersEngagementsForCompany };
+export {
+  getAllCompanies,
+  getCompanyById,
+  getUsersStatesFromModel,
+  getUsersEngagementsForCompany,
+  getUsersInterestsForCompany,
+};
