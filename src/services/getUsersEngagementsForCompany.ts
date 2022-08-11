ccd65f752c8ae03369c4d00263181a35d57c63b0
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-08-11 10:41:32 +0300

    commit

--- src/services/getUsersEngagementsForCompany.ts
+++ src/services/getUsersEngagementsForCompany.ts
@@ -1,7 +1,9 @@
 import { Company, User, UserEngagement } from '../database/models';
 
-const getUsersEngagementsForCompany = async (companyId: number): Promise<object> => {
-  const usersEngagements = await Company.findByPk(companyId, {
+const getUsersEngagementsForCompany = async (
+  companyId: number,
+): Promise<object> => {
+  const companyData = await Company.findByPk(companyId, {
     attributes: ['id'],
     include: [
       {
@@ -17,23 +19,58 @@ const getUsersEngagementsForCompany = async (companyId: number): Promise<object>
     ],
   });
 
+  const engagementsForAllUsers: [] = companyData?.toJSON().Users.map(
+    (user: {
+      UserEngagements: Array<{
+        engagements: [];
+      }>;
+    }) => ({
+      engagements: user.UserEngagements[0].engagements,
+    }),
+  );
+
+  const allEngagementsArray: string[] = [];
+  const engagementsCounters: { [key: string]: number } = {};
+
+  engagementsForAllUsers.forEach(
+    (engagementsForOneUser: { engagements: [] }) => {
+      allEngagementsArray.push(...engagementsForOneUser.engagements);
+    },
+  );
+
+  allEngagementsArray.forEach((engagement) => {
+    const key = engagement.toLowerCase().split(' ').join('_');
+
+    if (engagementsCounters[key]) {
+      engagementsCounters[key] += 1;
+    } else {
+      engagementsCounters[key] = 1;
+    }
+  });
+  const arr = Object.entries(engagementsCounters).map((element) => ({
+    counter: Math.round((element[1] / allEngagementsArray.length) * 100),
+    label: element[0]
+      .split('_')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' '),
+  }));
+
+  const sortedEngagements = arr.sort((a, b) => b.counter - a.counter);
+  const topThreeEngagements = sortedEngagements.slice(0, 3);
+  const othersEngagements = sortedEngagements
+    .slice(3)
+    .reduce((acc, curr) => acc + curr.counter, 0);
+
+  const totalEngagements = [
+    ...topThreeEngagements,
+    { counter: othersEngagements, label: 'Others' },
+  ];
+
   return {
     company: {
-      id: usersEngagements?.getDataValue('id'),
-      name: usersEngagements?.getDataValue('name'),
-      usersEngagements: usersEngagements?.toJSON().Users.map(
-        (user: {
-        UserEngagements: Array<{
-          id: number;
-          engagements: [];
-          user: number;
-        }>;
-      }) => ({
-          id: user.UserEngagements[0].id,
-          engagements: user.UserEngagements[0].engagements,
-          user: user.UserEngagements[0].user,
-        }),
-      ),
+      id: companyData?.getDataValue('id'),
+      name: companyData?.getDataValue('name'),
+      totalEngagements,
     },
   };
 };
