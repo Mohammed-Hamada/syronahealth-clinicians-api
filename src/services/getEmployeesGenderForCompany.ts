fbe70168675ab1354423f3f30d8b45e60b3fded6
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-08-13 13:19:27 +0300

    Create company employees genders controller

--- src/services/getEmployeesGenderForCompany.ts
+++ src/services/getEmployeesGenderForCompany.ts
@@ -1,4 +1,5 @@
 import { Employee, User } from '../database/models';
+import CustomError from '../helpers';
 
 const getEmployeesGenderForCompany = async (
   companyId: number,
@@ -14,8 +15,38 @@ const getEmployeesGenderForCompany = async (
     },
     attributes: ['gender'],
   });
+  if (!genderData.length) {
+    throw new CustomError(`There is no company with id ${companyId}`, 400);
+  }
 
-  return genderData;
+  const allGendersArray: string[] = [];
+  genderData.forEach((user) => {
+    allGendersArray.push(user.toJSON().gender);
+  });
+  const genderCounters: { [key: string]: number } = {};
+  allGendersArray.forEach((gender) => {
+    const key = gender.toLowerCase().split(' ').join('_');
+
+    if (genderCounters[key]) {
+      genderCounters[key] += 1;
+    } else {
+      genderCounters[key] = 1;
+    }
+  });
+  const arr = Object.entries(genderCounters).map((element) => ({
+    count: Math.round((element[1] / allGendersArray.length) * 100),
+    label: element[0]
+      .split('_')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' '),
+  }));
+
+  return {
+    company: {
+      id: companyId,
+      employeesGender: arr,
+    },
+  };
 };
 
 export default getEmployeesGenderForCompany;
