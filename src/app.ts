da05f80094dd035650a0b15fc4157b32fa835581
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-09-12 13:38:55 +0300

    Protect API with jwtAuthz middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,26 @@ import cors from 'cors';
 import morgan from 'morgan';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import { notFoundHandler, errorsHandler } from './middlewares';
+import axios from 'axios';
+import { notFoundHandler, errorsHandler, jwtCheck } from './middlewares';
 import router from './routes';
 
 const app: Express = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
-app.use(compression());
-app.use(
+app.use([
+  express.urlencoded({ extended: false }),
+  express.json(),
+  cookieParser(),
+  cors(),
+  compression(),
   morgan('tiny', {
     skip: () => process.env.NODE_ENV === 'production',
   }),
-);
+  jwtCheck,
+]);
+
 app.use('/api/v1', router);
-app.use(notFoundHandler);
-app.use(errorsHandler);
+
+app.use([notFoundHandler, errorsHandler]);
 
 export default app;
