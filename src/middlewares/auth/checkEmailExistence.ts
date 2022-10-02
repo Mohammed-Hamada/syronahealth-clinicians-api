1a89fe3f2dc3c7bbda93079ac3699fcf0be039e8
Mohammed <mohammed@pop-os.localdomain>
2022-10-02 22:43:09 +0300

    add emails to test production app

--- src/middlewares/auth/checkEmailExistence.ts
+++ src/middlewares/auth/checkEmailExistence.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { CustomError } from '../../helpers';
-import { getUserByEmail } from '../../services';
+import { getUserByEmail, updateUserByEmail } from '../../services';
 
 const checkEmailExistence = async (
   request: Request | any,
@@ -11,13 +11,13 @@ const checkEmailExistence = async (
   try {
     const { email } = request.auth;
     const user = await getUserByEmail(email);
-
     if (!user) {
       throw new CustomError(
         "Your email doesn't exists in our database",
         StatusCodes.BAD_REQUEST,
       );
     } else {
+      await updateUserByEmail(email);
       response.locals.user = user;
       next();
     }
